1f80025746c5af8ef66340c0827c869d698cd9ec
G43R1EL03 <gabriel.rodriguez11@utp.ac.pa>
2023-11-14 19:53:02 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,31 +70,31 @@ const getOptionChart2 = () => {
                 name: "Adecuacion Funcional",
                 type: "line",
                 stack: "Total",
-                data: [120, 132, 101, 134, 90, 230, 210]
+                data: [120, 132, 101, 134, 90, 230]
             },
             {
                 name: "Eficiencia de Desempeño",
                 type: "line",
                 stack: "Total",
-                data: [220, 182, 191, 234, 290, 330, 310]
+                data: [220, 182, 191, 234, 290, 330]
             },
             {
                 name: "Compatibilidad",
                 type: "line",
                 stack: "Total",
-                data: [150, 232, 201, 154, 190, 330, 410]
+                data: [150, 232, 201, 154, 190, 330]
             },
             {
                 name: "Usabilidad",
                 type: "line",
                 stack: "Total",
-                data: [320, 332, 301, 334, 390, 330, 320]
+                data: [320, 332, 301, 334, 390, 330]
             },
             {
                 name: "Fiabilidad",
                 type: "line",
                 stack: "Total",
-                data: [820, 932, 901, 934, 1290, 1330, 1320]
+                data: [820, 932, 901, 934, 1290, 1330]
             }
         ]
     };
