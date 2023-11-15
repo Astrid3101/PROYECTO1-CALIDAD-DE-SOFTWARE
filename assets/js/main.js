ba6e83759e3bbae49f6aab3ce5c2db4f6b3ab41a
G43R1EL03 <gabriel.rodriguez11@utp.ac.pa>
2023-11-14 19:24:58 -0500

    Udate resultados

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,7 +37,7 @@ const getOptionChart2 = () => {
             trigger: "axis"
         },
         legend: {
-            data: ["Email", "Union Ads", "Video Ads", "Direct", "Search Engine"]
+            data: ["Adecuacion Funcional", "Eficiencia de Desempeño", "Compatibilidad", "Usabilidad", "Fiabilidad"]
         },
         grid: {
             left: "3%",
@@ -54,7 +54,7 @@ const getOptionChart2 = () => {
             {
                 type: "category",
                 boundaryGap: false,
-                data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+                /*data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],*/
                 axisLine: { show: false },
                 axisTick: { show: false },
                 axisPointer: { type: "shadow" }
@@ -67,31 +67,31 @@ const getOptionChart2 = () => {
         ],
         series: [
             {
-                name: "Email",
+                name: "Adecuacion Funcional",
                 type: "line",
                 stack: "Total",
                 data: [120, 132, 101, 134, 90, 230, 210]
             },
             {
-                name: "Union Ads",
+                name: "Eficiencia de Desempeño",
                 type: "line",
                 stack: "Total",
                 data: [220, 182, 191, 234, 290, 330, 310]
             },
             {
-                name: "Video Ads",
+                name: "Compatibilidad",
                 type: "line",
                 stack: "Total",
                 data: [150, 232, 201, 154, 190, 330, 410]
             },
             {
-                name: "Direct",
+                name: "Usabilidad",
                 type: "line",
                 stack: "Total",
                 data: [320, 332, 301, 334, 390, 330, 320]
             },
             {
-                name: "Search Engine",
+                name: "Fiabilidad",
                 type: "line",
                 stack: "Total",
                 data: [820, 932, 901, 934, 1290, 1330, 1320]
@@ -156,11 +156,11 @@ const getOptionChart4 = () => {
                     show: false
                 },
                 data: [
-                    { value: 1048, name: "Search Engine" },
-                    { value: 735, name: "Direct" },
-                    { value: 580, name: "Email" },
-                    { value: 484, name: "Union Ads" },
-                    { value: 300, name: "Video Ads" }
+                    { value: 1048, name: "Seguridad" },
+                    { value: 735, name: "Mantenibilidad" },
+                    { value: 580, name: "Portabilidad" }
+                    /*{ value: 484, name: "Union Ads" },
+                    { value: 300, name: "Video Ads" }*/
                 ]
             }
         ]
